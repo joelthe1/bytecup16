7f5c9aaa105151a070e21bceeed2f8a1a134156d
Joe Mathai <joemathai16@gmail.com>
2016-11-14 08:00:26 +0000

    lightFM code

--- bin/matrix_factorization/hybrid_cf.py
+++ bin/matrix_factorization/hybrid_cf.py
@@ -14,25 +14,113 @@ from scipy import sparse
 from lightfm import LightFM
 from lightfm.evaluation import auc_score
 
+import numpy as np
+import pandas as pd
+import sys
+import copy
+import cPickle
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans, SpectralClustering
 
-NUM_THREADS = 20
-NUM_COMPONENTS = 30
-NUM_EPOCHS = 3
+NUM_THREADS = 24
+NUM_COMPONENTS = 320
+NUM_EPOCHS = 20
 ITEM_ALPHA = 1e-6
 
 question_path = "../../data/question_info.txt"
 user_path = "../../data/user_info.txt"
 invited_info_path = "../../data/invited_info_train.txt"
 
-
 q_column_names = ['q_id', 'q_tag', 'q_word_seq', 'q_char_seq', 'q_no_upvotes', 'q_no_answers', 'q_no_quality_answers']
 u_column_names = ['u_id','e_expert_tags', 'e_desc_word_seq', 'e_desc_char_seq']
 train_info_column_names = ['q_id','u_id','answered']
 
-print 'reading data...'
 question_dataframe = pd.read_csv(question_path, names=q_column_names, sep = '\t')
 user_dataframe = pd.read_csv(user_path, names = u_column_names, sep = '\t')
 train_info_dataframe = pd.read_csv(invited_info_path, names = train_info_column_names, sep = '\t')
+
+
+all_word_desc_list = question_dataframe['q_word_seq'].tolist() + user_dataframe['e_desc_word_seq'].tolist()
+all_char_desc_list = question_dataframe['q_char_seq'].tolist() + user_dataframe['e_desc_char_seq'].tolist()
+all_topics_list = question_dataframe['q_tag'].tolist() + user_dataframe['e_expert_tags'].tolist()
+
+word_vocabulary = set([word for sent in all_word_desc_list for word in str(sent).split('/')])
+#possible inconsistency in the data ---- char seq np.nan found !
+char_vocabulary = set([char for sent in all_char_desc_list for char in str(sent).split('/')])
+topic_vocabulary = set([char for sent in all_topics_list for char in str(sent).split('/')])
+
+cv_word = CountVectorizer(vocabulary=word_vocabulary, token_pattern=u'(?u)\\b\\w+\\b', ngram_range=(1,2))
+cv_char= CountVectorizer(vocabulary=char_vocabulary, token_pattern=u'(?u)\\b\\w+\\b', ngram_range=(3,5))
+cv_tag = CountVectorizer(vocabulary=topic_vocabulary, token_pattern=u'(?u)\\b\\w+\\b')
+
+word_counts = cv_word.fit_transform(question_dataframe['q_word_seq'].tolist())
+tf_word = TfidfTransformer(use_idf=True, norm='l2').fit(word_counts)
+word_tfs = tf_word.transform(word_counts)
+tsvd = TruncatedSVD(n_components=150, n_iter=7, random_state=42)
+word_tfs = tsvd.fit_transform(word_tfs)
+
+
+char_counts = cv_char.fit_transform(question_dataframe['q_char_seq'].tolist())
+tf_char = TfidfTransformer(use_idf=True, norm='l2').fit(char_counts)
+char_tfs = tf_char.transform(char_counts)
+tsvd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+char_tfs = tsvd.fit_transform(char_tfs)
+
+
+tag_counts = cv_tag.fit_transform([str(i) for i in question_dataframe['q_tag'].tolist()])
+tf_tags = TfidfTransformer(use_idf=True, norm='l2').fit(tag_counts)
+tag_tfs = tf_tags.transform(tag_counts)
+tsvd = TruncatedSVD(n_components=50, n_iter=7, random_state=42)
+tag_tfs = tsvd.fit_transform(tag_tfs)
+
+
+question_feature_matrix = Normalizer(copy=True).fit_transform(np.hstack([word_tfs, char_tfs, tag_tfs]))
+
+
+
+question_feature_map = {}
+question_features = []
+for i, q in question_dataframe.iterrows():
+    # question_feature_map[q['q_id']] = np.hstack([question_feature_matrix[i,:], q['q_no_upvotes'],
+    #                                              q['q_no_answers'], q['q_no_quality_answers']])
+    question_features.append(np.hstack([question_feature_matrix[i,:], q['q_no_upvotes'],
+                                        q['q_no_answers'], q['q_no_quality_answers']]))
+
+# USER FEATURES
+
+u_column_names = ['u_id','e_expert_tags', 'e_desc_word_seq', 'e_desc_char_seq']    
+word_counts = cv_word.fit_transform(user_dataframe['e_desc_word_seq'].tolist())
+tf_word = TfidfTransformer(use_idf=True, norm='l2').fit(word_counts)
+word_tfs = tf_word.transform(word_counts)
+tsvd = TruncatedSVD(n_components=150, n_iter=7, random_state=42)
+word_tfs = tsvd.fit_transform(word_tfs)
+
+
+char_counts = cv_char.fit_transform(user_dataframe['e_desc_char_seq'].tolist())
+tf_char = TfidfTransformer(use_idf=True, norm='l2').fit(char_counts)
+char_tfs = tf_char.transform(char_counts)
+tsvd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+char_tfs = tsvd.fit_transform(char_tfs)
+
+
+tag_counts = cv_tag.fit_transform([str(i) for i in user_dataframe['e_expert_tags'].tolist()])
+tf_tags = TfidfTransformer(use_idf=True, norm='l2').fit(tag_counts)
+tag_tfs = tf_tags.transform(tag_counts)
+tsvd = TruncatedSVD(n_components=50, n_iter=7, random_state=42)
+tag_tfs = tsvd.fit_transform(tag_tfs)
+
+user_feature_matrix = Normalizer(copy=True).fit_transform(np.hstack([word_tfs, char_tfs, tag_tfs]))
+
+user_feature_map = {}
+user_features = []
+for i, u in user_dataframe.iterrows():
+    #user_feature_map[u['u_id']] = np.hstack([user_feature_matrix[i,:]])
+    user_features.append(np.hstack([user_feature_matrix[i,:]]))
 
 print 'creating index map...'
 q_index_map = {qid:i for i, qid in enumerate(question_dataframe['q_id'].tolist())}
@@ -47,39 +135,48 @@ for i, t in train_info_dataframe.iterrows():
 X = np.array(X)
 y = np.array(y)
 
-#feature vector dict
-print 'reading feature vectors...'
-q_features = cPickle.load(open( "../../data/question_features.pkl", "rb" ))
-u_features = cPickle.load(open( "../../data/user_features.pkl", "rb" ))
 
 
-skf = StratifiedKFold(n_splits=9)
-i = 0
+skf = StratifiedKFold(n_splits=10)
+ci = 0
+auc = []
 for train_index, test_index in skf.split(X, y):
-    print 'Cross-Validation run:{}, train_size:{}, test_size:{}'.format(i+1,len(train_index), len(test_index))
-    i+=1
+    print 'Cross-Validation run:{}'.format(ci+1)
+    ci+=1
     Xtrain, ytrain = X[train_index], y[train_index]
     Xtest, ytest = X[test_index], y[test_index]
-    train_matrix = np.zeros((len(user_dataframe), len(question_dataframe)))
+    train_matrix = np.zeros((len(user_dataframe), len(question_dataframe)), dtype=float)
     user_train_features = []
+    user_test_features = []
     question_train_features = []
-    for i,X in enumerate(Xtrain):
-        train_matrix[u_index_map[X[0]]][q_index_map[X[1]]] = int(ytrain[i])
-        user_train_features.append(u_features[X[0]])
-        question_train_features.append(q_features[X[1]])
+    question_test_features = []
+    for i,XV in enumerate(Xtrain):
+        train_matrix[u_index_map[XV[0]]][q_index_map[XV[1]]] = int(ytrain[i])
     model = LightFM(loss='warp',
                     item_alpha=ITEM_ALPHA,
                     no_components=NUM_COMPONENTS)
-    model.fit(sparse.csr_matrix(train_matrix), epochs=NUM_EPOCHS, num_threads=NUM_THREADS)
+    model.fit(sparse.csr_matrix(train_matrix, dtype=np.float32),
+              user_features=sparse.csr_matrix(user_features, dtype=np.float32),
+              item_features=sparse.csr_matrix(question_features, dtype=np.float32),
+              epochs=NUM_EPOCHS, num_threads=NUM_THREADS)
 
     Xtest_u_ids, Xtest_q_ids = zip(*Xtest)
-    Xtest_u_ids = [u_index_map[id] for id in Xtest_u_ids]
-    Xtest_q_ids = [q_index_map[id] for id in Xtest_q_ids]
-    ypred = model.predict(Xtest_u_ids, Xtest_q_ids)
-
-    train_auc = auc_score(model, train, num_threads=NUM_THREADS).mean()
+    Xtest_u_ids = np.array([u_index_map[id] for id in Xtest_u_ids])
+    Xtest_q_ids = np.array([q_index_map[id] for id in Xtest_q_ids])
+    print model.user_embeddings.shape, sparse.csr_matrix(user_features).shape
+    ypred = model.predict(Xtest_u_ids, Xtest_q_ids,
+                          user_features = sparse.csr_matrix(user_features, dtype=np.float32),
+                          item_features = sparse.csr_matrix(question_features, dtype=np.float32),
+                          num_threads = NUM_THREADS)
+                          
+    train_auc = auc_score(model, sparse.csr_matrix(train_matrix, dtype=np.float32),
+                          user_features = sparse.csr_matrix(user_features, dtype=np.float32),
+                          item_features = sparse.csr_matrix(question_features, dtype=np.float32),
+                          num_threads=NUM_THREADS).mean()
     test_auc = roc_auc_score(ytest, ypred)
-    print 'CF train AUC:{}'.format(train_auc)
-    
+    auc.append(test_auc)
+    print 'CF train AUC:{} test AUC:{}'.format(train_auc, test_auc)
+
+print 'mean AUC on test: {}'.format(np.mean(auc))
     
     

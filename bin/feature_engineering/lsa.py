00e71660aae054b03eb89325bb19ea4044c061f5
chinmaya-gautam <chinmaya.gautam@gmail.com>
2016-11-16 02:10:48 +0000

    changes.

---
+++ bin/feature_engineering/lsa.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+import sys
+import copy
+import cPickle
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans, SpectralClustering
+
+# first 3 for questions and next 3 for user features
+def run(ncomp=[500, 500, 20, 700, 700, 20]):
+    question_path = "../../data/question_info.txt"
+    user_path = "../../data/user_info.txt"
+    invited_info_path = "../../data/invited_info_train.txt"
+
+    q_column_names = ['q_id', 'q_tag', 'q_word_seq', 'q_char_seq', 'q_no_upvotes', 'q_no_answers', 'q_no_quality_answers']
+    u_column_names = ['u_id','e_expert_tags', 'e_desc_word_seq', 'e_desc_char_seq']
+    train_info_column_names = ['q_id','u_id','answered']
+
+    question_dataframe = pd.read_csv(question_path, names=q_column_names, sep = '\t')
+    user_dataframe = pd.read_csv(user_path, names = u_column_names, sep = '\t')
+    train_info_dataframe = pd.read_csv(invited_info_path, names = train_info_column_names, sep = '\t')
+
+
+    all_word_desc_list = question_dataframe['q_word_seq'].tolist() + user_dataframe['e_desc_word_seq'].tolist()
+    all_char_desc_list = question_dataframe['q_char_seq'].tolist() + user_dataframe['e_desc_char_seq'].tolist()
+    all_topics_list = question_dataframe['q_tag'].tolist() + user_dataframe['e_expert_tags'].tolist()
+
+    word_vocabulary = set([word for sent in all_word_desc_list for word in str(sent).split('/')])
+    #possible inconsistency in the data ---- char seq np.nan found !
+    char_vocabulary = set([char for sent in all_char_desc_list for char in str(sent).split('/')])
+    topic_vocabulary = set([char for sent in all_topics_list for char in str(sent).split('/')])
+
+
+    print "Size of the word vocabulary :", len(word_vocabulary)
+    print "Size of the char vocabulary :", len(char_vocabulary)
+    print "Number of topics : ", len(topic_vocabulary)
+
+
+    cv_word = CountVectorizer(vocabulary=word_vocabulary, token_pattern=u'(?u)\\b\\w+\\b', ngram_range=(1,2))
+    cv_char= CountVectorizer(vocabulary=char_vocabulary, token_pattern=u'(?u)\\b\\w+\\b', ngram_range=(3,5))
+    cv_tag = CountVectorizer(vocabulary=topic_vocabulary, token_pattern=u'(?u)\\b\\w+\\b')
+
+    word_counts = cv_word.fit_transform(question_dataframe['q_word_seq'].tolist())
+    tf_word = TfidfTransformer(use_idf=True, norm='l2').fit(word_counts)
+    word_tfs = tf_word.transform(word_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[0], n_iter=7, random_state=42)
+    word_tfs = tsvd.fit_transform(word_tfs)
+    print "question word features", word_tfs.shape
+
+    char_counts = cv_char.fit_transform(question_dataframe['q_char_seq'].tolist())
+    tf_char = TfidfTransformer(use_idf=True, norm='l2').fit(char_counts)
+    char_tfs = tf_char.transform(char_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[1], n_iter=7, random_state=42)
+    char_tfs = tsvd.fit_transform(char_tfs)
+    print "question char features", char_tfs.shape
+
+    tag_counts = cv_tag.fit_transform([str(i) for i in question_dataframe['q_tag'].tolist()])
+    tf_tags = TfidfTransformer(use_idf=True, norm='l2').fit(tag_counts)
+    tag_tfs = tf_tags.transform(tag_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[2], n_iter=7, random_state=42)
+    tag_tfs = tsvd.fit_transform(tag_tfs)
+    print "question tag features", tag_tfs.shape
+
+    question_feature_matrix = Normalizer(copy=True).fit_transform(np.hstack([word_tfs, char_tfs, tag_tfs]))
+    print question_feature_matrix
+    print "question_feature_matrix original shape: ", question_feature_matrix.shape
+
+
+#    kmeans = KMeans(n_clusters=7, random_state=0).fit(question_feature_matrix)
+#    question_clusters1 = kmeans.labels_
+#    sclustering = SpectralClustering(n_clusters=15, gamma=0.1).fit(question_feature_matrix)
+#    question_clusters2 = sclustering.labels_
+
+
+    question_feature_map = {}
+    for i, q in question_dataframe.iterrows():
+        question_feature_map[q['q_id']] = np.hstack([question_feature_matrix[i,:], q['q_no_upvotes'],
+                                                     q['q_no_answers'], q['q_no_quality_answers']])
+
+
+#    question_feature_matrix = []
+#    question_feature_map.clear()
+
+    # USER FEATURES
+
+    u_column_names = ['u_id','e_expert_tags', 'e_desc_word_seq', 'e_desc_char_seq']    
+    word_counts = cv_word.fit_transform(user_dataframe['e_desc_word_seq'].tolist())
+    tf_word = TfidfTransformer(use_idf=True, norm='l2').fit(word_counts)
+    word_tfs = tf_word.transform(word_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[3], n_iter=7, random_state=42)
+    word_tfs = tsvd.fit_transform(word_tfs)
+    print "user word features", word_tfs.shape
+
+    char_counts = cv_char.fit_transform(user_dataframe['e_desc_char_seq'].tolist())
+    tf_char = TfidfTransformer(use_idf=True, norm='l2').fit(char_counts)
+    char_tfs = tf_char.transform(char_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[4], n_iter=7, random_state=42)
+    char_tfs = tsvd.fit_transform(char_tfs)
+    print "user char features", char_tfs.shape
+
+    tag_counts = cv_tag.fit_transform([str(i) for i in user_dataframe['e_expert_tags'].tolist()])
+    tf_tags = TfidfTransformer(use_idf=True, norm='l2').fit(tag_counts)
+    tag_tfs = tf_tags.transform(tag_counts)
+    tsvd = TruncatedSVD(n_components=ncomp[5], n_iter=7, random_state=42)
+    tag_tfs = tsvd.fit_transform(tag_tfs)
+    print "user tag features", tag_tfs.shape
+
+    user_feature_matrix = Normalizer(copy=True).fit_transform(np.hstack([word_tfs, char_tfs, tag_tfs]))
+    print user_feature_matrix
+    print "user_feature_matrix original shape: ", user_feature_matrix.shape
+
+#    kmeans = KMeans(n_clusters=7, random_state=0).fit(user_feature_matrix)
+#    user_clusters1 = kmeans.labels_
+#    sclustering = SpectralClustering(n_clusters=15, gamma=0.1).fit(user_feature_matrix)
+#    user_clusters2 = sclustering.labels_
+
+    user_feature_map = {}
+    for i, u in user_dataframe.iterrows():
+        user_feature_map[u['u_id']] = np.hstack([user_feature_matrix[i,:]])
+
+    print len(question_feature_map), len(user_feature_map)
+    return question_feature_map, user_feature_map
+
+
+if __name__ == '__main__':
+    question_feature_map, user_feature_map = run()
+    
+    with open('question_features.pkl','wb') as fp:
+        cPickle.dump(question_feature_map,fp)
+    
+    with open('user_features.pkl','wb') as fp:
+        cPickle.dump(user_feature_map,fp)
--- bin/lightgbm/massage_data.py
+++ bin/lightgbm/massage_data.py
@@ -2,39 +2,59 @@ import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix, hstack, vstack
 import pickle
+import sys
+sys.path.insert(0, '~/zmod/bin/feature_engineering')
+import lsa
 
 train_info_dataframe = pd.read_csv("../../data/invited_info_train.txt", names = ['q_id','u_id','answered'], sep = '\t')
 validate_ids_dataframe = pd.read_pickle("../../data/validate_nolabel.pkl")
 
-print "\nLoading pca data..."
-print "Loading user data..."
-u_dict = pickle.load(open("user_features.pkl",'r'))
-print "Loading user data(complete)"
-print "Loading question data..."
-q_dict = pickle.load(open("question_features.pkl",'r'))
-print "Loading question data(complete)"
+print "\n Loading data mapings"
+q_dict, u_dict = lsa.run()
+print "Loading data mapings(complete)"
 
 print "\ncombining both user and question data..."
 
-feat_ids = range(1,844,1)
+for i,data_source in enumerate([train_info_dataframe, validate_ids_dataframe]):
+    if i==0:
+        continue
+    res_bclass = []
+    res_rank = []
+    res_reg = []
 
-res_rank = []
-res_reg = []
-for idx, entry in validate_ids_dataframe.iterrows():
-    line = hstack([q_dict[entry['q_id']], u_dict[entry['u_id']]]).toarray()
+    for idx, entry in data_source.iterrows():
+        line = hstack([q_dict[entry['q_id']], u_dict[entry['u_id']]]).toarray()
 
-    # Regression
-    x_reg = map(lambda x: '{:.18f}'.format(x), line[0])
-    res_reg.append('1\t' + '\t'.join(x_reg))
-#    res_reg.append(str(entry['answered']) + '\t' + '\t'.join(x_reg))
+        # Binary classification
+        x_bclass = map(lambda x: '{:.18f}'.format(x), line[0])
+        if i == 0:
+            res_bclass.append(str(entry['answered']) + '\t' + '\t'.join(x_bclass))  #training data
+        else:
+            res_bclass.append('1\t' + '\t'.join(x_bclass))                          #test data
+        
+        # Regression
+        #x_reg = map(lambda x: '{:.18f}'.format(x), line[0])
+        #if i == 0:
+        #    res_reg.append('1\t' + '\t'.join(x_reg))
+        #else:
+        #    res_reg.append(str(entry['answered']) + '\t' + '\t'.join(x_reg))
 
-    # Rank
-    x = map(lambda (i, x): '{}:{:.18f}'.format((i+1), x) if x!=0 else 0, enumerate(line[0]))
-    x = filter(lambda x: type(x) == str, x)
-    t = '1\t' + '\t'.join(x)
-#    t = str(entry['answered']) + '\t' + '\t'.join(x)
-    res_rank.append(t)
+        # Rank
+        #x = map(lambda (i, x): '{}:{:.18f}'.format((i+1), x) if x!=0 else 0, enumerate(line[0]))
+        #x = filter(lambda x: type(x) == str, x)
+        #t = '1\t' + '\t'.join(x)
+        #    t = str(entry['answered']) + '\t' + '\t'.join(x)
+        # res_rank.append(t)
 
+    if i == 0:
+        wfile = open('bien_bclassifier.train', 'w')
+        wfile.write('\n'.join(res_bclass))
+        wfile.close()
+    else:
+        wfile = open('bien_bclassifier.test', 'w')
+        wfile.write('\n'.join(res_bclass))
+        wfile.close
+'''
 wfile = open('bien_rank.test', 'w')
 wfile.write('\n'.join(res_rank))
 wfile.close()
@@ -42,6 +62,7 @@ wfile.close()
 wfile = open('bien_regression.test', 'w')
 wfile.write('\n'.join(res_reg))
 wfile.close()
+'''
 #X = csr_matrix(vstack(tempX))
 #save_sparse_csr("../../data/csr_mat_train_lsa.dat", X)
 

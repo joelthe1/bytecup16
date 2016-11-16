acdace7f05598c328dcefc60fc89c0a16dd61371
joelthe1 <joelmath@usc.edu>
2016-11-16 02:36:44 +0000

    Fixing test file format.

--- bin/lightgbm/massage_data.py
+++ bin/lightgbm/massage_data.py
@@ -10,7 +10,8 @@ train_info_dataframe = pd.read_csv("../../data/invited_info_train.txt", names = 
 validate_ids_dataframe = pd.read_pickle("../../data/validate_nolabel.pkl")
 
 print "\n Loading data mapings"
-q_dict, u_dict = lsa.run()
+n_comp = [500, 500, 20, 700, 700, 20]
+q_dict, u_dict = lsa.run(n_comp)
 print "Loading data mapings(complete)"
 
 print "\ncombining both user and question data..."
@@ -30,19 +31,19 @@ for i,data_source in enumerate([train_info_dataframe, validate_ids_dataframe]):
         if i == 0:
             res_bclass.append(str(entry['answered']) + '\t' + '\t'.join(x_bclass))  #training data
         else:
-            res_bclass.append('1\t' + '\t'.join(x_bclass))                          #test data
+            res_bclass.append('\t'.join(x_bclass))                          #test data
         
         # Regression
         #x_reg = map(lambda x: '{:.18f}'.format(x), line[0])
         #if i == 0:
-        #    res_reg.append('1\t' + '\t'.join(x_reg))
+        #    res_reg.append('\t'.join(x_reg))
         #else:
         #    res_reg.append(str(entry['answered']) + '\t' + '\t'.join(x_reg))
 
         # Rank
         #x = map(lambda (i, x): '{}:{:.18f}'.format((i+1), x) if x!=0 else 0, enumerate(line[0]))
         #x = filter(lambda x: type(x) == str, x)
-        #t = '1\t' + '\t'.join(x)
+        #t = '\t'.join(x)
         #    t = str(entry['answered']) + '\t' + '\t'.join(x)
         # res_rank.append(t)
 
@@ -63,8 +64,6 @@ wfile = open('bien_regression.test', 'w')
 wfile.write('\n'.join(res_reg))
 wfile.close()
 '''
-#X = csr_matrix(vstack(tempX))
-#save_sparse_csr("../../data/csr_mat_train_lsa.dat", X)
 
 print "combining data and save(complete)"
 print "\nLoading pca data(complete)"

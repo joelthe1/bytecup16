58e75f4ed2b77e517cc766b46ce569cd4a1e7d22
Joe Mathai <joemathai16@gmail.com>
2016-11-20 01:54:50 +0000

    lightGBM grid improvements

--- bin/lightgbm/lightGBM_grid.py
+++ bin/lightgbm/lightGBM_grid.py
@@ -8,33 +8,51 @@ from feature_engineering.load_data import loadData
 execs = "~/LightGBM/lightgbm"
 data = loadData('../../data')
 
-X, Y = data.training_features()
+X, Y, Xval, Xtest = data.dataset()
 
-gbm = GBMClassifier(exec_path=execs,
-                    metric='binary_error')
+gbm = GBMClassifier(exec_path=execs)
 
-param_grid = {'learning_rate': [0.1, 0.04],
-              'bagging_fraction': [0.5, 0.9],
-              'num_iterations':[100, 150, 180, 250],
-              'num_leaves' : [100, 127, 150],
-              'lambda_l1':[0, .1],
-              'lambda_l2':[0, .3, 3],
-              'max_depth':[-1, 10, 12, 14],
-              'max_bin':[255, 350, 450],
-              'feature_fraction':[1, .6, .3, .1],
+param_grid = {'application':['binary'],
+              'learning_rate': [0.05],
+              'bagging_fraction': [1, 0.8],
+              'num_iterations': [550],
+              'num_leaves' : [50, 127],
+              'lambda_l1':[0, .3],
+              'lambda_l2':[0, .3],
+              'max_depth':[8, 10, 12],
+              'max_bin':[255],
+              'feature_fraction':[1],
               'tree_learner':['serial', 'feature', 'data'],
-              'boosting_type':['gbdt', 'dart'],
-              'bagging_fraction':[1],
+              'boosting_type':['dart'],
+              'bagging_fraction':[.8],
               'bagging_freq':[10],
-              'early_stopping_round':[10, 15],
-              'drop_rate':[0.01, 0.1, 0.03],
+              'early_stopping_round':[10],
+              'drop_rate':[0.01],
               'num_threads':[24]
               }
               
 scorer = metrics.make_scorer(metrics.roc_auc_score, greater_is_better=True)
-clf = model_selection.GridSearchCV(gbm, param_grid, scoring=scorer, cv=2)
-
-clf.fit(X, Y)
-
-print("Best score: ", clf.best_score_)
+clf = model_selection.GridSearchCV(gbm, param_grid, scoring=scorer, cv=5)
+clf.fit(X,Y)
 print("Best params: ", clf.best_params_)
+
+
+
+# write test file
+y_pred = clf.predict(Xtest)
+with open('test_output.txt','w') as f:
+    f.write('qid,uid,label\n')
+    for i, test in data.test.iterrows():
+        f.write('{},{},{}\n'.format(test['q_id'],test['u_id'],y_pred[i]))
+# write valid file        
+y_pred = clf.predict(Xval)
+with open('valid_output.txt','w') as f:
+    f.write('qid,uid,label\n')
+    for i, test in data.validation.iterrows():
+        f.write('{},{},{}\n'.format(test['q_id'],test['u_id'],y_pred[i]))
+
+
+
+
+
+
